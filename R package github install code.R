d546ed74d9c927ec40183397f85d6d493146f5a3
DEBASHIS CHATTERJEE <72778655+debashischatterjee111@users.noreply.github.com>
2020-11-11 02:13:22 +0530

    d1

---
+++ R package github install code.R
@@ -0,0 +1,13 @@
+#installation
+
+require(devtools)
+install_github("debashischatterjee111/BPviGM1")
+
+
+install.packages("devtools")
+require(devtools)
+install_github("debashischatterjee111/BPviGM1")
+
+install.packages("githubinstall")
+require(githubinstall)
+githubinstall("BPviGM1")
